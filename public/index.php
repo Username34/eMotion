61ca124337f6b07cb508f8af2f3087725dbc056a
Mehdi Bouziane <mbouziane@welcome-media.fr>
2017-09-29 14:47:55 +0200

    fixup! fix

--- public/index.php
+++ public/index.php
@@ -1,9 +1,9 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
-require '../src/model/Vehicles.php';
-require '../src/model/Offers.php';
-require '../src/model/Users.php';
+require __DIR__ .'../src/model/Vehicles.php';
+require __DIR__ .'../src/model/Offers.php';
+require __DIR__ .'../src/model/Users.php';
 session_start();
 
 // Instantiate the app
